c1fe4df9a20c6e3ba9593ba7844ac8c6def62eef
vorisekmartin <martin.vorisek@kiwi.com>
2019-02-20 23:20:10 +0100

    refresh posts on home press

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -9,6 +9,7 @@ import NextButton from "./NextButton"
 import { nextStateSelector, ctkStateSelector, ctkPostSelector } from "../selectors/ctkSelectors"
 import { setCtkState } from "../actions/ctkActions"
 import { CTK_STATES } from "../reducers/ctkConstants"
+import { fetchCTKPosts } from "../scripts/fetchCTKPosts"
 
 const styles = StyleSheet.create({
   container: {
@@ -37,37 +38,47 @@ const styles = StyleSheet.create({
   },
 })
 
-const handlePress = ({ dispatch, nextState, state, post }) => {
-  if (fp.isEqual(nextState)(state)) {
-    if (state.index === 0) alert("Chybí vybraný post - klikni na článek, který chceš publikovat")
-    if (state.index === 2)
-      alert("Chybí hlavní kategorie - zvolí se dlouhým kliknutím na již vybranou kategorii")
-    if (state.index === 4)
-      alert(
-        `Chybí úvodní obrázek - zvolí se dlouhým kliknutím na již vybraný obrázek nebo není dostatečný počet obrázků: mmusí být vybráno ${parseInt(
-          post.image_count,
-        ) + 1}`,
-      )
+class TopBar extends React.Component {
+  handlePress = () => {
+    const { dispatch, nextState, state, post } = this.props
+    if (fp.isEqual(nextState)(state)) {
+      if (state.index === 0) alert("Chybí vybraný post - klikni na článek, který chceš publikovat")
+      if (state.index === 2)
+        alert("Chybí hlavní kategorie - zvolí se dlouhým kliknutím na již vybranou kategorii")
+      if (state.index === 4)
+        alert(
+          `Chybí úvodní obrázek - zvolí se dlouhým kliknutím na již vybraný obrázek nebo není dostatečný počet obrázků: mmusí být vybráno ${parseInt(
+            post.image_count,
+          ) + 1}`,
+        )
+    }
+    dispatch(setCtkState(nextState))
   }
-  dispatch(setCtkState(nextState))
+
+  handleReturnToHome = () => {
+    const { dispatch } = this.props
+    dispatch(setCtkState(CTK_STATES.DEFAULT))
+    dispatch(fetchCTKPosts())
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.logoWrapper}>
+          <TouchableOpacity onPress={this.handleReturnToHome}>
+            <Icon style={styles.icon} color="#2a3f53" name="home" size={22} />
+          </TouchableOpacity>
+          <MonoText style={styles.title}>oEnergetice.cz</MonoText>
+        </View>
+        <NextButton
+          style={{ height: 32, width: 100, padding: 0 }}
+          handlePressNext={this.handlePress}
+        />
+      </View>
+    )
+  }
 }
 
-const TopBar = props => {
-  return (
-    <View style={styles.container}>
-      <View style={styles.logoWrapper}>
-        <TouchableOpacity onPress={() => props.dispatch(setCtkState(CTK_STATES.DEFAULT))}>
-          <Icon style={styles.icon} color="#2a3f53" name="home" size={22} />
-        </TouchableOpacity>
-        <MonoText style={styles.title}>oEnergetice.cz</MonoText>
-      </View>
-      <NextButton
-        style={{ height: 32, width: 100, padding: 0 }}
-        handlePressNext={() => handlePress(props)}
-      />
-    </View>
-  )
-}
 export default connect(state => ({
   post: ctkPostSelector(state),
   state: ctkStateSelector(state),
